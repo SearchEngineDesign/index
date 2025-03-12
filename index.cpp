69a83f5c48cdcfc1eb024f216785f0b388608f97
otherseas1 <97400272+tkmaher@users.noreply.github.com>
2025-03-11 23:17:29 -0400

    Updated index files

--- index.cpp
+++ index.cpp
@@ -2,6 +2,119 @@
 
 #include "index.h"
 
-Index::Index() {
-    
+void Index::addDocument(HtmlParser &parser) {
+   Tuple<string, PostingList> *seek;
+   string concat;
+   for (auto i : parser.bodyWords) {
+      seek = dict.Find(i.first, PostingList(i.first, Token::Body));
+      seek->value.appendBodyDelta(i.second + WordsInIndex, 0);
+   }
+   for (auto i : parser.headWords) {
+      seek = dict.Find(i.first, PostingList(i.first, Token::Body));
+      seek->value.appendBodyDelta(i.second + WordsInIndex, 3);
+   }
+   for (auto i : parser.boldWords) {
+      seek = dict.Find(i.first, PostingList(i.first, Token::Body));
+      seek->value.appendBodyDelta(i.second + WordsInIndex, 2);
+   }
+   for (auto i : parser.italicWords) {
+      seek = dict.Find(i.first, PostingList(i.first, Token::Body));
+      seek->value.appendBodyDelta(i.second + WordsInIndex, 1);
+   }
+   for (auto i : parser.titleWords) {
+      concat = titleMarker + i.first;
+      seek = dict.Find(concat, PostingList(concat, Token::Title));
+      seek->value.appendTitleDelta(i.second + WordsInIndex);
+   }
+
+   for (auto i : parser.links) {
+      //TODO: implement a better way to index anchor text
+      bool first = true;
+      for (auto j : i.anchorText) {
+         if (first) {
+            //places URL delta at beginning of corresponding anchor text
+            concat = urlMarker + i.URL;
+            seek = dict.Find(concat, PostingList(concat, Token::URL));
+            seek->value.appendTitleDelta(j.second + WordsInIndex); 
+            first = false;
+         }
+         concat = anchorMarker + j.first;
+         seek = dict.Find(concat, PostingList(concat, Token::Anchor));
+         seek->value.appendTitleDelta(j.second + WordsInIndex);
+      }     
+   }
+   seek = dict.Find(eodMarker, PostingList(eodMarker, Token::EoD));
+   seek->value.appendEODDelta(parser.count + WordsInIndex, DocumentsInIndex);
+   
+   WordsInIndex += parser.count;
+   DocumentsInIndex += 1;
+   documents.push_back(parser.base);
+}
+
+//for utillity
+uint8_t bitsNeeded(const size_t n) {
+    if (n == 0) {
+        return 1; 
+    }
+    return std::max(1, static_cast<int>(std::ceil(std::log2(n + 1))));
+}
+
+char *formatUtf8(const size_t &delta) {
+   const uint8_t boundary = bitsNeeded(delta);
+   size_t bytes = 0;
+   if (boundary < 7)
+      bytes = 1;
+   else if (boundary < 12)
+      bytes = 2;
+   else if (boundary < 17)
+      bytes = 3;
+   else if (boundary < 22)
+      bytes = 4;
+   else if (boundary < 27)
+      bytes = 5;
+   else if (boundary < 32)
+      bytes = 6;
+   else if (boundary < 37)
+      bytes = 7;
+
+   char* bitset = new char[bytes];
+   uint8_t bitsetIndex = 0, initDelta = 0, deltaIndex = 0, index = bytes;
+   
+   while(deltaIndex < boundary) { 
+      if (bitsetIndex % 8 == 0)
+         index--;
+      initDelta = deltaIndex + 6;
+      for (; deltaIndex < initDelta && deltaIndex < boundary; deltaIndex++) {
+         if ((delta >> deltaIndex) & 1)
+            bitset[index] |= 1 << bitsetIndex;
+         bitsetIndex++;
+      }
+      if (deltaIndex < boundary) {
+         bitset[index] |= 0 << bitsetIndex;
+         bitset[index] |= 1 << (bitsetIndex + 1);
+         bitsetIndex += 2;
+      }
+      bitsetIndex = bitsetIndex % 8;
+   }
+   for (int i = 7; i > 7 - bytes; i--)
+      bitset[0] |= 1 << i;
+
+   return bitset;
+}
+
+void PostingList::appendTitleDelta(const size_t delta) {
+   list.emplace_back(formatUtf8(delta));  
+}
+
+void PostingList::appendBodyDelta(size_t delta, uint8_t style) {
+   delta = delta << 2;
+   delta += style;
+   list.emplace_back(formatUtf8(delta)); 
+}
+
+void PostingList::appendEODDelta(size_t delta, size_t docIndex) {
+   //TODO: tweak how we process these
+   delta = delta << sizeof(docIndex);
+   delta += docIndex;
+   list.emplace_back(formatUtf8(delta));  
 }
--- index.h
+++ index.h
@@ -1,9 +1,17 @@
 //index.h
 
-#include <cstddef>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <cmath>
 #include <bitset>
-#include "include/string.h"
-#include "include/vector.h"
+
+#include "../include/string.h"
+#include "../include/vector.h"
+#include "../include/Utf8.h"
+#include "hash/HashTable.h"
+#include "../parser/HtmlParser.h"
 
 enum class Token {
     EoD,            //end-of-document token
@@ -20,24 +28,100 @@ enum class Style {
     Bold        //represented by 10
 };
 
-struct Post {
+typedef size_t Location; // Location 0 is the null location.
+typedef size_t FileOffset;
+
+class Post {
+private:
     //Variable byte size char array, to be encoded in utf-8.
     //Structure: n bits to encode the offset + 2 bits to encode style (for word tokens)
-    //n bits to encode the offset + log(count) bits to encode anchor text???? consult slides
+    //n bits to encode the offset + log(count) bits to number of times the anchor text 
+        //the word belongs to has occurred with the URL it links to (for anchor text)
     //n bits to encode the EOF + n bits to encode an index to the corresponding URL for EOF tokens
     char *data;
+
+    int get_bytes(const char first_byte) {
+        uint8_t bytes = 0;
+        uint8_t sentinel = 7;
+        while (first_byte >> sentinel & 1) {
+            bytes++;
+            sentinel--;
+        }
+        return bytes;
+    }
+public:
+   
+    Post() {
+        data = nullptr;
+    }
+
+    Post(const char * data_in) {
+        int bytes = get_bytes(data_in[0]);
+        data = new char[bytes];
+        memcpy(data, data_in, bytes);
+        delete[] data_in;
+    }
+
+    ~Post() {
+        if (data != nullptr)
+            delete[] data;
+    }
+
+    Post &operator=(const Post &other) {
+        if (data != nullptr)
+            delete[] data;
+        int bytes = get_bytes(other.data[0]);
+        data = new char[bytes];
+        memcpy(data, other.data, bytes);
+        return *this;
+    }
+
+    void printBits() {
+        for (int i = 0; i < get_bytes(data[0]); i++) {
+            for (int j = 7; j >= 0; j--) {
+                std::cout << (data[i] >> j & 1);
+            }
+            std::cout << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    Style getStyle() {
+        //TODO
+        return Style::Normal;
+    }
+
+    Location getDelta() {
+        //TODO
+        return 0;
+    }
 };
 
 class PostingList {
 public:
+    //virtual Post *Seek( Location );
+    void appendTitleDelta(size_t delta); //title token
+    void appendBodyDelta(size_t delta, uint8_t style); //body token
+    void appendEODDelta(size_t delta, size_t docIndex); //EOF token
+
+    //Construct empty posting list for string str_in
+    PostingList(string &str_in, Token type_in) : 
+                index(str_in), type(type_in), useCount(1) {}
+
+    //Get size of post list (in bytes)
+    size_t getListSize() {
+       return sizeof(list);
+    }
+
+    string getIndex() {
+        return index;
+    }
 
 private:
-
     //Common header
     size_t useCount;        //number of times token occurs
     size_t documentCount;   //number of documents containing token
     Token type;             //variety of token
-    size_t listSize;        //size of list (in bytes)
 
     //Type-specific data
 
@@ -53,7 +137,40 @@ private:
 
 class Index {
 public:
-    Index();
-private:
+    //Constructor should take in parsed HTML and add it to the index.
+    void addDocument(HtmlParser &parser);
+    vector<string> documents;
+    size_t WordsInIndex = 0, 
+    DocumentsInIndex = 0;
 
+    Index() {}
+
+    HashTable<string, PostingList> *getDict() {
+        return &dict;
+    }
+
+private:
+    //HashBlob dictBlob;
+    HashTable<string, PostingList> dict;
+
+    string titleMarker = string("@");
+    string anchorMarker = string("$");
+    string urlMarker = string("#");
+    string eodMarker = string("%");
+};
+
+class ISR {
+public:
+    virtual Post *Next( );
+    virtual Post *NextDocument( );
+    virtual Post *Seek( Location target );
+    virtual Location GetStartLocation( );
+    virtual Location GetEndLocation( );
+};
+
+class ISRWord : public ISR {
+public:
+    unsigned GetDocumentCount( );
+    unsigned GetNumberOfOccurrences( );
+    virtual Post *GetCurrentPost( );
 };
